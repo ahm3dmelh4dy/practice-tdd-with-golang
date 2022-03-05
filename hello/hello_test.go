f9f114e2349a2d699ef56458fd163045071c62cc
Ahmed Elhady <a.m.elhadyyy@gmail.com>
2022-03-05 20:09:15 +0200

    refactor hello module to use subtests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,9 +5,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Ahmed")
-	want := "Hello, Ahmed"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Ahmed")
+		want := "Hello, Ahmed"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
